c5a305510938c1c2478880717456918fb6c0ee3c
markjesusjh <143747566+markjesusjh@users.noreply.github.com>
2023-10-17 17:20:20 -0500

    Update GroupRepository.java

--- demo/demo/src/main/java/com/example/demo/entidad1/GroupRepository.java
+++ demo/demo/src/main/java/com/example/demo/entidad1/GroupRepository.java
@@ -5,12 +5,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 }
-
-/*
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
-*/
