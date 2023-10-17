d6c5bf7e8062d015581ab8ea7d29e67eabd3dfcc
markjesusjh <143747566+markjesusjh@users.noreply.github.com>
2023-10-17 17:29:48 -0500

    Delete demo/demo directory

--- demo/demo/src/main/java/com/example/demo/entidad1/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.entidad1;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
