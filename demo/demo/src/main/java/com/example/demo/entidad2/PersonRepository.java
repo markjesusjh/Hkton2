4ffa5f1f90f8dfcf4bc835aa7d9efd69370ddc80
markjesusjh <143747566+markjesusjh@users.noreply.github.com>
2023-10-17 17:20:56 -0500

    Update PersonRepository.java

--- demo/demo/src/main/java/com/example/demo/entidad2/PersonRepository.java
+++ demo/demo/src/main/java/com/example/demo/entidad2/PersonRepository.java
@@ -1,8 +1,3 @@
-//import com.example.hack_2.E_2.Person;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//package com.example.hack_2.E_2;
-
 package com.example.demo.entidad2;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
